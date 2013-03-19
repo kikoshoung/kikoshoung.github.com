22dcfd8ccc06af143de641a5d0c2ac78b6ccd18b
kikoshoung <kiko.xiang@guohead.com>
2013-03-19 17:44:41 +0800

    modi

--- js/component/ad-killer-install.js
+++ js/component/ad-killer-install.js
@@ -7,12 +7,12 @@
 	var script = document.createElement('script'), 
 		panel = document.createElement('div'),
 		body = document.body,
-		mode = 0, // 0: dev, 1: online
+		mode = 1, // 0: dev, 1: online
 		domain = !mode ? 'http://localhost' : 'http://kikoshoung.me';
 
 	if(window.location.protocol === 'https:') domain = 'https://raw.github.com/kikoshoung/kikoshoung.github.com/master';
 
-	script.src = domain + '/js/component/ad-killer.js';
+	script.src = domain + '/dist/component/ad-killer.js';
 
 	panel.id = 'ad-killer-panel',
 	panel.style.cssText = 'position: fixed; z-index: 9999999999; top: 0; right: 0; padding: 5px 10px; background-color: white; color: black; font-size: 12px;';

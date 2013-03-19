52557b4cd3139a3ea0c302b25de67763eb1edfe3
kikoshoung <kiko.xiang@guohead.com>
2013-03-19 22:05:02 +0800

    ad-killer page's referrence chenged

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ var site = {
 			titleFragment: " | Radarcharts"
 		},
 		"#ad-killer": {
-			fileName: "adKiller",
+			fileName: "https://raw.github.com/kikoshoung/ad-killer/master/index",
 			titleFragment: " | 广告杀手-插件"
 		}
 	},
